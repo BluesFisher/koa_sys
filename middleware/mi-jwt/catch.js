883e475fdf17ebf37551f4d35f25e177c841cb7a
adamsyu <adamsyu@tencent.com>
2020-04-03 11:11:07 +0800

    update sso

--- middleware/mi-jwt/catch.js
+++ middleware/mi-jwt/catch.js
@@ -3,6 +3,7 @@ module.exports = app => (ctx, next) =>
     if (err.status === 401) {
       ctx.status = 401;
       ctx.body = { code: 401, msg: "未登陆", data: {} };
+      return;
     } else {
       throw err;
     }
