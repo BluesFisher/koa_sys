4bbd2dc9ac24940879a50d5d44430a14c8c28031
adamsyu <adamsyu@tencent.com>
2020-04-18 15:39:13 +0800

    modify node_log

--- controller/logInfo.js
+++ controller/logInfo.js
@@ -2,13 +2,14 @@ module.exports = {
   getLogItem: async (ctx, next) => {
     let { id } = ctx.request.body;
     let res = await ctx.app.lib.sqlQuery(
-      "select * from t_node_log where id=?",
-      [id || 0]
+      //   "select * from t_node_log where id=?",
+      "select * FROM t_node_log where `date` between '2020-03-30 19:43:57' and '2020-03-30 20:21:38'"
     );
-    if (res.length === 1) {
-      ctx.send({ code: 0, message: "success", data: res[0] });
+    if (res.length >= 1) {
+      ctx.send({ code: 0, message: "success", data: res });
+      return;
     } else {
-      ctx.send({ code: -1, message: "failed", data: res[0] });
+      ctx.send({ code: -1, message: "failed", data: res });
     }
   },
 };
